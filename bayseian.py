7001de21b09c843263ba44faedab3857c1288944
roselily <rararose5555@gmail.com>
2025-10-28 20:32:55 +0530

    bayseian.py

--- bayseian.py
+++ bayseian.py
@@ -1,13 +1,13 @@
 from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
-# Define Bayesian Network
+
 model = DiscreteBayesianNetwork([
 ('Weather', 'Humidity'),
 ('Weather', 'PlayTennis'),
 ('Humidity', 'PlayTennis')
 ])
-# CPTs
+
 cpd_weather = TabularCPD(variable='Weather', variable_card=2,
 values=[[0.6], [0.4]],
 state_names={'Weather': ['Sunny', 'Rainy']})
@@ -18,7 +18,7 @@ evidence=['Weather'],
 evidence_card=[2],
 state_names={'Humidity': ['High', 'Normal'],
 'Weather': ['Sunny', 'Rainy']})
-# Corrected PlayTennis CPT
+
 cpd_play = TabularCPD(variable='PlayTennis', variable_card=2,
 values=[[0.4, 0.9, 0.1, 0.5], # Yes
 [0.6, 0.1, 0.9, 0.5]], # No
@@ -27,10 +27,11 @@ evidence_card=[2, 2],
 state_names={'PlayTennis': ['Yes', 'No'],
 'Weather': ['Sunny', 'Rainy'],
 'Humidity': ['High', 'Normal']})
-# Add CPTs to model
+
 model.add_cpds(cpd_weather, cpd_humidity, cpd_play)
 assert model.check_model(), "Model is incorrect!"
-# Inference
+
 infer = VariableElimination(model)
 query_result = infer.query(variables=['PlayTennis'], evidence={'Weather': 'Sunny'})
+
 print(query_result)
